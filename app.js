b34b8335a031d4ca5527acce32aeb33fb33bbfa5
Pirathap <pirathap.dev@gmail.com>
2026-02-23 16:23:16 +0530

    promises added

--- app.js
+++ app.js
@@ -36,3 +36,17 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
 });
 
 console.log(myModule.myFunction());
+
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+myPromise
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
