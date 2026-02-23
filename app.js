14927812921a911e4efe34d2df167b346f10081b
Pirathap <pirathap.dev@gmail.com>
2026-02-23 16:05:33 +0530

    Web server created

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 
 fs.readFile('file.txt', 'utf8', function (err, data) {
   if (err) throw err;
@@ -10,3 +11,9 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err;
   console.log('File saved!');
 });
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
